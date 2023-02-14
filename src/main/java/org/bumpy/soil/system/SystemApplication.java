879959b482fc0f04513d46aafaf82da5bd48b310
gaojg <echo_cat@126.com>
2023-02-14 09:28:06 +0800

    新建SpringBoot工程

---
+++ src/main/java/org/bumpy/soil/system/SystemApplication.java
@@ -0,0 +1,16 @@
+package org.bumpy.soil.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bumpy soil
+ */
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+
+}
